61922d2e4bc15cf643e5c9ba835a12b5e8476d02
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-05 20:46:06 +0530

    Single player at start position 0

--- SnakeandLadderProblem/SnakeandLadderProblem/Program.cs
+++ SnakeandLadderProblem/SnakeandLadderProblem/Program.cs
@@ -5,6 +5,7 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Snake And ladder Program");
+            StartPosition.startPosition();
         }
     }
 }
---
+++ SnakeandLadderProblem/SnakeandLadderProblem/StartPosition.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SnakeandLadderProblem
+{
+    public class StartPosition
+    {
+        public static void startPosition()
+        {
+            int position = 0;
+            int player;
+
+            
+            Console.WriteLine("Player is at start position : " + position);
+        }
+    }
+}
